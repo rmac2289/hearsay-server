85cccafa43c02db840ec008412ad13da0e93b1cb
Ross MacDonald <rmac2289@gmail.com>
2020-05-26 10:21:56 -0700

    added user_name to reviews

--- src/reviews/reviews-router.js
+++ src/reviews/reviews-router.js
@@ -26,6 +26,7 @@ reviewsRouter
         })
         
       newReview.user_id = req.user.id
+      newReview.user_name = req.user_name.id
 
     ReviewsService.insertReview(
       req.app.get('db'),
